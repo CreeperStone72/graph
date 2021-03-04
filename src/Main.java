133a6f7f1854f6be5b5a860956de4a8ce7110e85
CreeperStone72 <55103485+CreeperStone72@users.noreply.github.com>
2021-03-04 21:37:54 +0100

    Finalization for updated .jar
    Now with SimpleGraph

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,5 @@ import simple.SimpleGraph;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleGraph graph = new SimpleGraph();
-
-        for(int i = 0 ; i < 10 ; i++)
-            graph.insert(i);
-
-        try { graph.link(3, 5); }
-        catch(NodeNotFoundException e) { e.printStackTrace(); }
-
-        System.out.println(graph.getLinks());
-        System.out.println(graph.getNodes());
-        System.out.println(graph.size());
-        System.out.println(graph.order());
     }
 }
